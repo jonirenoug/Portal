85d13000218b7daa1b4cbaa38013f5d648d9e400
Nhat Huynh Van <jonirenoug@gmail.com>
2018-08-24 02:31:43 +0700

    Try connected db

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	if (DB::connection('myDamnDbConnection')->getDatabaseName()){
+	   return 'Connected to the DB: ' . DB::connection('myDamnDbConnection')->getDatabaseName();
+	} else {
+		return view('welcome');
+	}
 });
